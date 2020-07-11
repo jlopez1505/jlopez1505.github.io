842cb59bf133e9695ef44ffc6bb3ff80e6f38b7e
Jonathan López <jonathan_lopez15@outlook.com>
2020-07-10 19:24:27 -0500

    animaciones comlpletas home y responsive texto falso de relleno

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
-let intro = document.getElementById("intro");
+let intro = document.getElementById("titulo");
 let copy = document.getElementById("copy");
+let servicios = document.getElementById("services");
+let nav = document.getElementsByClassName("nav-bar");
 let moving = false;
 
 function scroll() {
-    if (window.pageYOffset > 10) {
+    let posicionHome = copy.getBoundingClientRect().top
+    let posicionServicios = servicios.getBoundingClientRect().top
+    console.log(posicionHome)
+    
+    
+    if (window.scrollY > 1) {
         intro.classList.add("move-titulo")
-    copy.classList.add("move")   
-    } else {
+        copy.classList.add("move")
+        if(posicionServicios < 200){
+            console.log(posicionServicios)
+            home.classList.add("relativo")
+            nav[0].style.position = "absolute"
+        } 
+        else {
+            home.classList.remove("relativo")
+            nav[0].style.position = "fixed" 
+        } 
+    } 
+        else {
         intro.classList.remove("move-titulo")
         copy.classList.remove("move") 
-    }
+    } 
 }
+
