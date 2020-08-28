c2afac063852d929f5c7eb39793a66b9fbf4b242
Jonathan López <jonathan_lopez15@outlook.com>
2020-08-27 22:52:37 -0500

    Seccion servicios mark 1

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let intro = document.getElementById("titulo");
 let copy = document.getElementById("copy");
 let servicios = document.getElementById("services");
 let nav = document.getElementsByClassName("nav-bar");
+let home = document.getElementById("home")
 let moving = false;
 
 function scroll() {
@@ -9,7 +10,7 @@ function scroll() {
     let posicionServicios = servicios.getBoundingClientRect().top
     console.log(posicionHome)
     
-    
+//cambiar los style por clases para agregar animacion
     if (window.scrollY > 1) {
         intro.classList.add("move-titulo")
         copy.classList.add("move")
@@ -17,10 +18,12 @@ function scroll() {
             console.log(posicionServicios)
             home.classList.add("relativo")
             nav[0].style.position = "absolute"
+            servicios.style.marginTop = "0"
         } 
         else {
             home.classList.remove("relativo")
-            nav[0].style.position = "fixed" 
+            nav[0].style.position = "fixed"
+            servicios.style.marginTop = "50%"
         } 
     } 
         else {
